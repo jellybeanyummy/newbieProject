54f08af96244035c212acf0b13f140bd4f7c8396
jellybean <davidshin01@kaist.ac.kr>
2019-09-01 11:02:15 +0000

    for err

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Register, Login, App } from './containers/index';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from 'reducers';
+import reducers from './reducers/index';
 import thunk from 'redux-thunk';
 
 const store = createStore(reducers, applyMiddleware(thunk));
